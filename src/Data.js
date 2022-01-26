e7dc87db9ee01fc3f841a1bd9333d987ca284e32
AdamHrioui1 <adamhrioui1@gmail.com>
2022-01-26 23:59:24 +0100

    k

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,88 @@
-import React from 'react';
+const Data = [
+  {
+    id: 1,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 2,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 3,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 4,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 5,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 6,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 7,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 8,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 9,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 10,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 11,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 12,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 13,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+  {
+    id: 14,
+    image: '../imgs/img0.jpg',
+    title: 'title one',
+    price: 10
+  },
+]
 
-function Data() {
-  return <div>
-      <h1>Data</h1>
-  </div>;
-}
-
-export default Data;
+export default Data
--- src/Slider.js
+++ src/Slider.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import data from './Data'
 
 function Slider() {
   return <div>
       <h1>Slider</h1>
+      <div className="wrapper">
+        <div className="item">
+          <div className="img_container">
+            <img src="../imgs/img0.jpg" />
+          </div>
+
+          <p>hello everyone, this is a random text </p>
+        </div>
+      </div>  
+
   </div>;
 }
 
