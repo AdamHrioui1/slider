2e92ddae5ba4842ef7b5ef78d906a45298b05706
AdamHrioui1 <adamhrioui1@gmail.com>
2022-01-27 14:04:55 +0100

    third commit

--- src/Item.js
+++ src/Item.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
-function Item({ data, counter }) {
+function Item({ data, counter, width }) {
   const { id, title, price, image } = data
+  console.log(width);
 
-  return <div className="item" key={id} style={{ 'left': `${(id-counter)*20}vw`}}>
+  return <div className="item" key={id} style={{ 'left': `${(id-counter)*width}vw`, 'width': `${width}vw`, 'height': `${width}vw`}}>
     <div className="img_container">
       <img src="../imgs/img0.jpg" />
     </div>
 
     <p>hello everyone, this is a random text </p>
     <p>{id}</p>
-
 
   </div>
 }
--- src/Slider.js
+++ src/Slider.js
@@ -1,20 +1,70 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import data from './Data'
 import Item from './Item';
 
 function Slider() {
   const [counter, setCounter] = useState(0)
+  const [width, setWidth] = useState(20)
+  const [limitOfCounter, setLimitOfCounter] = useState(4)
+
+  window.addEventListener('resize', (e) => {
+    const w = e.target.innerWidth
+    if(w >= 900) {
+      setWidth(20)
+      setLimitOfCounter(5)
+    }
+    else if(w < 900 && w > 800) {
+      setWidth(25)
+      setLimitOfCounter(4)
+    }
+    else if(w < 800 && w > 600) {
+      setWidth(33)
+      setLimitOfCounter(3)
+    }
+    else if(w < 600 && w > 510) {
+      setWidth(50)
+      setLimitOfCounter(2)
+    }
+    else if(w < 510) {
+      setWidth(100)
+      setLimitOfCounter(1)
+    }
+})
+
+  useEffect(() => {
+    console.log('yep');
+  }, [width]);
+  
+
+  const increaseCounter = () => {
+    if(counter >= data.length - limitOfCounter) {
+      setCounter(counter)
+    }
+    else {
+      setCounter(counter+1)
+    }
+  }
+
+  const decreaseCounter = () => {
+    if(counter === 0) {
+      setCounter(0)
+    }
+    else {
+      setCounter(counter-1)
+    }
+  }
   return <div>
       <h1>Slider</h1>
       <div className="wrapper">
         {
           data.map((item, index) => {
-            return <Item key={index} data={item} counter={counter} />
+            return <Item key={index} data={item} counter={counter} width={width} />
           })
         }
       </div>
 
-      <button onClick={() => setCounter(counter+1)} >L</button>
+      <button onClick={decreaseCounter}>L</button>
+      <button onClick={increaseCounter}>R</button>
   </div>;
 }
 
